7c0388f4a659c3f1a79b62300388881cdbeff944
Anirudh Varanasi <anirudh838@gmail.com>
2019-06-08 13:21:18 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,5 @@
-
+# Construct the plot Time Vs Global Active power and save it to a PNG file with a 
+# width of 480 pixels and a height of 480 pixels
 plot(data$Time, data$Global_active_power, type = "l", 
      xlab = "", ylab = "Global Active Power (kilowatts)")
 dev.copy(png, file = "plot2.png", width = 480, height = 480)
